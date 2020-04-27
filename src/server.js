0dffe83575f6235f9fa574afdd3e2b8effb2ce60
Jean Junior <jclgjunior@gmail.com>
2020-04-27 14:54:02 -0300

    Server is ready

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const router = require('./routes');
+
+const app = express();
+app.use(cors());
+app.use(router);
+
+
+
+
+
+app.listen(3300, console.log('Running'));
